4793411a70e69994525ec99070c0ccdcab005584
maryaa1 <mariarumkorem07@gmail.com>
2024-07-12 00:13:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if st.button('Test Prediksi Air'):
     water_prediction = model_water_quality_prediction.predict(input_data)
     if water_prediction[0] == 1:
         water_prediction = 'Air dapat Diminum'
-         st.success(water_prediction)
+        st.success(water_prediction)
     else:
         water_prediction = 'Air Tidak dapat Diminum'
         st.error(water_prediction) 
