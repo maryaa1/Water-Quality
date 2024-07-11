c9cce973918ab626d68a979bfd887972bd7e2034
maryaa1 <mariarumkorem07@gmail.com>
2024-07-11 23:40:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ if st.button('Test Prediksi Air'):
     water_prediction = model_water_quality_prediction.predict(input_data)
     
     # Menginterpretasikan hasil prediksi
-    if water_prediction[0]==1:
+    if water_prediction[0]>0.5:
         # Jika probabilitas > 0.5, air dianggap dapat diminum
         water_prediction = 'Air dapat Diminum'
         st.success(water_prediction)  # Menampilkan hasil dengan pesan sukses
